b03701a0d7fce758e3284a2c5338fb99768ba265
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-01-09 00:44:47 +0900

    [add] first customization

--- build.ts
+++ build.ts
@@ -8,6 +8,8 @@ const firefoxFlag = process.argv.includes('--firefox');
 const builder = new Builder({ watchFlag, devFlag, chromeFlag, firefoxFlag });
 builder.addBuildFile('popup/index.tsx');
 builder.addStaticFile('popup/popup.html');
+builder.addStaticFile('contentscript.js');
+builder.addStaticFile('style.css');
 builder.addStaticDir('icons');
 
 builder.build();
