ddb0fa1a66c7f4ced64908d7208502bb0da11b21
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-01-13 14:16:45 +0900

    [fix] only when tab is defined

--- src/background.ts
+++ src/background.ts
@@ -1,24 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'deepl',
-    title: 'DeepL',
-    contexts: ['all'],
+    id: "deepl",
+    title: "DeepL",
+    contexts: ["all"],
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case 'deepl':
-      chrome.scripting.executeScript({
-        target: { tabId: tab?.id as number },
-        func: translate,
-      });
-      break;
-    default:
-      console.log('Default');
+  if (tab !== undefined) {
+    switch (info.menuItemId) {
+      case "deepl":
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id as number },
+          func: translate,
+        });
+        break;
+    }
   }
 });
 
 export const translate = () => {
-  console.log('Hello World');
-}
+  console.log("Hello World");
+};
